5e46b825d69629bb34262d81b1a02496c05db035
Godwin1603 <erroreditz823@gmail.com>
2025-02-19 12:09:48 +0530

    Added Firebase config and updated hostel submission page

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+const { initializeApp } = require("firebase/app");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTWsDNEAggzoIimEGrb8WuC_gtYhkaFpQ",
+  authDomain: "hostel-pg-976dc.firebaseapp.com",
+  projectId: "hostel-pg-976dc",
+  storageBucket: "hostel-pg-976dc.appspot.com",
+  messagingSenderId: "186169969854",
+  appId: "1:186169969854:web:f367d5c8fafaa05d8b62ef",
+  measurementId: "G-MFTCLBWTDH"
+};
+
+module.exports = firebaseConfig;
